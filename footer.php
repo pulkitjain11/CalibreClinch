b7393fb3ba6fef7395cba2063f58329b30fecbc9
Pulkit <pulkitjainjk@gmail.com>
2020-10-28 02:09:25 +0530

    Quiz Files from b2s

---
+++ footer.php
@@ -0,0 +1,24 @@
+<div class="footer-copyright-area" style="background-color:black;height:100px">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="footer-copy-right">
+                    <p>>Copyright &copy; 2020 CALIBRE CLINCH</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
+
+
+
+<script src="js/vendor/jquery-1.12.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
